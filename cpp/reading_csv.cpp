4d2736ee984d9a079481c86c5b45c46b2db24d1d
Soham-KT <115338084+Soham-KT@users.noreply.github.com>
2025-07-21 23:38:54 +0530

    something

--- cpp/reading_csv.cpp
+++ cpp/reading_csv.cpp
@@ -7,24 +7,26 @@ using namespace std;
 
 int main(){
     string filepath = "D:\\Codes\\Projects\\NN-from-scratch\\dataset\\dataset.csv";
-    ifstream data;
-
-    data.open(filepath);
-    if (data.fail()) {
-        cerr << "Error opening file: " << filepath << endl;
-        return 1;
-    }
+    string outputpath = "D:\\Codes\\Projects\\NN-from-scratch\\dataset\\hellokitty.txt";
+    ifstream data(filepath);
+    ofstream output(outputpath);
 
     bool firstLine = true;
     string records;
 
-    while(data.peek() != EOF){ 
+    while(data.peek() != EOF){
+        if (firstLine){
+          firstLine = false;
+          getline(data, records);
+          continue;  
+        }
+
         getline(data, records);
         if (records.empty()) continue; 
         
         cout << records << endl;
+        output << records << endl;
         cout << firstLine << endl;
-        cout << "-----------------------------------------------------------------" << endl;
     }
 
     return 0;
